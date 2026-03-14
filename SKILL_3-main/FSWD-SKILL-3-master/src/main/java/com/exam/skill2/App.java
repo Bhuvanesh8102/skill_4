82e56cdd330ab621a38a88b5c4dfd2e2346030e9
ecbhuvanesh <ecbhuvanesh@gmail.com>
2026-03-14 19:35:02 +0530

    first commit

---
+++ SKILL_3-main/FSWD-SKILL-3-master/src/main/java/com/exam/skill2/App.java
@@ -0,0 +1,13 @@
+package com.exam.skill2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
